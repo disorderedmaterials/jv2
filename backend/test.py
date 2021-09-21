81d0ca04c06f620502beca83b1d0fd1def8a3956
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2021-09-21 15:06:46 +0100

    Full packaging and deployment

---
+++ backend/test.py
@@ -0,0 +1,4 @@
+import subprocess
+
+p = subprocess.Popen("../isisTest/isisTest")
+q = subprocess.Popen("../jv2/jv2")
---
+++ version.h
@@ -0,0 +1,24 @@
+/*
+    *** JournalViewer2 version information
+    *** version.h
+    Copyright T. Youngs 2012-2016
+    This file is part of JournalViewer2.
+    JournalViewer2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    JournalViewer2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with JournalViewer2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef JOURNALVIEWER2_VERSION_H
+#define JOURNALVIEWER2_VERSION_H
+
+#define JV2VERSION "0.1"
+#define JV2URL "http://www.projectaten.net/jv2"
+
+#endif
