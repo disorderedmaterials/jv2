4fcf98417056856f497d603938c6f1c6a625308d
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2021-10-19 15:31:18 +0100

    have python launch from application bundle

--- backend/launch.py
+++ backend/launch.py
@@ -1,4 +1,13 @@
 import subprocess
+import sys
+import os
 
-isisInternal = subprocess.Popen("../isisInternal/isisInternal")
-frontend = subprocess.Popen("../jv2/jv2")
+if getattr(sys, 'frozen', False):
+    # we are running in a bundle
+    bundle_dir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+
+isisInternal = subprocess.Popen(os.path.join(bundle_dir, 'isisInternal'))
+frontend = subprocess.Popen(os.path.join(bundle_dir, 'jv2/jv2'))
