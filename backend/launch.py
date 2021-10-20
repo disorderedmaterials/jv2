0d67199ad6bdeab196070502c868119d3ad65835
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2021-10-20 13:23:32 +0100

    format test isDir

--- backend/launch.py
+++ backend/launch.py
@@ -12,7 +12,7 @@ else:
     # we are running in a normal Python environment
     bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
-while(os.path.isdir(bundle_dir) == False):
+while(os.path.isdir(bundle_dir) is False):
     continue
 isisInternal = subprocess.Popen(os.path.join(bundle_dir, 'isisInternal'))
 frontend = subprocess.Popen(os.path.join(bundle_dir, 'jv2/jv2'))
