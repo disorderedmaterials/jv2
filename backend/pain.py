a19b2c76bbb056c6d401d4a643be9c724dd68709
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2022-02-10 12:33:12 +0000

    pain

---
+++ backend/pain.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2022 E. Devlin and T. Youngs
+
+import os 
+
+import lxml
+
+path = os.path.abspath(lxml.__file__)
+print(path)
+#print(os.listdir(path))
+
