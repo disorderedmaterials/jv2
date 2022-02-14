75226e25762dd552491be094a2d294751eead91a
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2022-02-14 16:04:24 +0000

    test launch success

--- backend/launch.py
+++ backend/launch.py
@@ -2,11 +2,6 @@
 # Copyright (c) 2022 E. Devlin and T. Youngs
 
 import subprocess
-import os
-print(os.getcwd())
-os.chdir('/bin/launch')
-print(os.getcwd())
-isisInternal = subprocess.Popen('./isisInternal/isisInternal')
+
+isisInternal = subprocess.Popen('isisInternal/isisInternal')
 frontend = subprocess.Popen('../jv2')
-for i in range(1, 10):
-  print("")
