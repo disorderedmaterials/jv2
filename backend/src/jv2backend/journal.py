0ac948b5648e73e5399f2a1e05b8e2dd50bdde67
Martyn Gigg <martyn.gigg@gmail.com>
2022-10-25 13:33:08 +0100

    Initial testing layout and minimal running

---
+++ backend/src/jv2backend/journal.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2022 E. Devlin, M. Gigg and T. Youngs
+"""Defines a Journal class to encapsulate a Journal for user cycle"""
+
+class Journal:
+    pass
---
+++ backend/src/jv2backend/tests/test_journal.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2022 E. Devlin, M. Gigg and T. Youngs
+"""Tests for the Journal class"""
+from jv2backend.journal import Journal
+
+
+def test_journal_creation():
+    journal = Journal()
+
+    assert(True)
