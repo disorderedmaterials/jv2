36c9df10d477c7eb31fb1b09f50c83b2ba70a6be
Martyn Gigg <martyn.gigg@gmail.com>
2022-10-26 11:54:09 +0100

    Add basics of a Journal class
    It is simply associated with a collection
    of Runs and an instrument so far

--- backend/src/jv2backend/tests/test_journal.py
+++ backend/src/jv2backend/tests/test_journal.py
@@ -1,9 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2022 E. Devlin, M. Gigg and T. Youngs
+from jv2backend.cycle import Cycle
+from jv2backend.instrument import Instrument
 from jv2backend.journal import Journal
 
 
-def test_journal_creation():
-    journal = Journal()
+def test_default_journal_creation_stores_cycle_and_instrument_and_contains_zero_runs():
+    instrument = Instrument("fake")
+    cycle = Cycle(2021, 1)
+    journal = Journal(instrument, cycle)
 
-    assert True
+    assert journal.instrument == instrument
+    assert journal.cycle == cycle
+    assert journal.run_count() == 0
