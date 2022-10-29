3aa899e22dbde007cbba94c66c5b2b96e1f74a75
Martyn Gigg <martyn.gigg@gmail.com>
2022-10-29 07:44:54 +0100

    Rewrite Run for easier attribute extension

--- backend/src/jv2backend/run.py
+++ backend/src/jv2backend/run.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2022 E. Devlin, M. Gigg and T. Youngs
 """Capture Run information"""
+from __future__ import annotations
 import dataclasses
 
 from jv2backend.experiment import Experiment
@@ -12,25 +13,24 @@ class Run:
     """Capture an experiment Run information"""
 
     # Define the attributes from the Run captured from the Journal
-    run_number: int
+    name: str
+    run_number: str
     experiment: Experiment
     instrument: Instrument
-    # __slots__ = ("run_number", "experiment", "instrument")
 
     def __init__(self, **kwargs) -> None:
         """Create an object from some or all of the given keywords. They must
         be one of those specified in the __slots__ field. Additional values
-        are annoyed.
+        are annoyed. Keys/Types are not checked. It is reccommended that factory
+        methods such as from_dict are used.
         """
-        missing, type_errors = [], []
+        missing = []
         for field in dataclasses.fields(self.__class__):
             name = field.name
             value = kwargs.get(name, None)
             if value is None:
                 missing.append(name)
                 continue
-            if not isinstance(value, field.type):
-                type_errors.append(name)
             # accept the value
             setattr(self, name, value)
 
@@ -39,8 +39,34 @@ class Run:
                 f"Cannot create Run. Missing the following fields: " + ",".join(missing)
             )
 
-        if type_errors:
-            raise TypeError(
-                f"Cannot create Run. The following fields have unexpected types: "
-                + ",".join(type_errors)
-            )
+
+#         for name, value in kwargs.items():
+#             setattr(self, name, value)
+
+
+# class RunBuilder:
+#     """Logic to build a Run type and convert from strings to correct field types"""
+
+#     def __init__(self):
+#         self._fields: dict = {}
+
+#     def add_attrs(self, **kwargs) -> None:
+#         """Set attributes to pass to the created Run object.
+#         This can be called many times to add further values before calling build
+#         The types are converted if they do not match those expected by the Run
+#         object
+
+#         :param kwargs: Dict of attributes defining a Run
+#         """
+#         self._fields.update(kwargs)
+
+#     def build(self) -> Run:
+#         """Take preset field values, attempt to convert to expected types
+#         and build the Run.
+
+#         :raises ValueError: If fields are missing
+#         :raises TypeError: If field types cannot be converted
+#         :return: A new Run object
+#         """
+#         # build!
+#         return Run(**run_fields)
