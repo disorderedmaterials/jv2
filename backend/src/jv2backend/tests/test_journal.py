3bbc3a0a7f71814b7b78bd3c09b31e47062ed484
Martyn Gigg <martyn.gigg@gmail.com>
2022-11-14 09:41:52 +0000

    Extend search tests across other fields

--- backend/src/jv2backend/tests/test_journal.py
+++ backend/src/jv2backend/tests/test_journal.py
@@ -10,15 +10,24 @@ from jv2backend.journal import Journal
 INSTRUMENT_NAME = "FAKE"
 
 
+@pytest.fixture()
+def journal_fake(sample_journal_dataframe):
+    return _create_fake_journal(sample_journal_dataframe)
+
+
+def _create_fake_journal(sample_dataframe):
+    return Journal(Instrument(INSTRUMENT_NAME), sample_dataframe)
+
+
 def test_default_journal_creation_stores_instrument_and_data(sample_journal_dataframe):
-    journal = _create_test_journal(sample_journal_dataframe)
+    journal = _create_fake_journal(sample_journal_dataframe)
 
     assert journal.instrument.name == INSTRUMENT_NAME
     assert journal.run_count == len(sample_journal_dataframe)
 
 
 def test_to_json_gives_dataframe_with_same_information(sample_journal_dataframe):
-    journal = _create_test_journal(sample_journal_dataframe)
+    journal = _create_fake_journal(sample_journal_dataframe)
 
     runs = journal.to_json()
 
@@ -34,11 +43,11 @@ def test_to_json_gives_dataframe_with_same_information(sample_journal_dataframe)
     "case_sensitive",
     [True, False],
 )
-def test_search_by_user_name_field(case_sensitive, sample_journal_dataframe):
-    journal = _create_test_journal(sample_journal_dataframe)
+def test_search_by_user_name_field_uses_a_contains_check_not_exact_match(
+    case_sensitive, journal_fake
+):
     run_field, user_input = "user_name", "username"
-
-    search_results = journal.search(run_field, user_input, case_sensitive)
+    search_results = journal_fake.search(run_field, user_input, case_sensitive)
 
     if case_sensitive:
         assert search_results.run_count == 0
@@ -46,19 +55,35 @@ def test_search_by_user_name_field(case_sensitive, sample_journal_dataframe):
         assert search_results.run_count == 3
 
 
-# def test_extend_adds_the_runs_from_the_given_journal_with_caller(
-#     sample_journal_dataframe,
-# ):
-#     journal_a, journal_b = _create_test_journal(
-#         sample_journal_dataframe
-#     ), _create_test_journal(sample_journal_dataframe)
-#     journal_a_run_count_orig = journal_a.run_count
+@pytest.mark.parametrize(
+    "case_sensitive",
+    [True, False],
+)
+def test_search_by_experiment_identifier_uses_exact_string_matching(
+    case_sensitive, journal_fake
+):
+    run_field, user_input = "experiment_identifier", "123456"
 
-#     journal_a.extend(journal_b)
+    search_results = journal_fake.search(run_field, user_input, case_sensitive)
+    assert search_results.run_count == 0
 
-#     assert journal_a.run_count == journal_a_run_count_orig + journal_b.run_count
+    user_input = "1234567"
+    search_results = journal_fake.search(run_field, user_input, case_sensitive)
+    assert search_results.run_count == 2
 
 
-# Private helpers
-def _create_test_journal(sample_data: pd.DataFrame) -> Journal:
-    return Journal(Instrument("FAKE"), sample_data)
+@pytest.mark.parametrize(
+    "case_sensitive",
+    [True, False],
+)
+def test_search_by_title_uses_a_contains_check_and_not_exact_match(
+    case_sensitive, journal_fake
+):
+    run_field, user_input = "title", "My cycle"
+
+    search_results = journal_fake.search(run_field, user_input, case_sensitive)
+
+    if case_sensitive:
+        assert search_results.run_count == 0
+    else:
+        assert search_results.run_count == 1
