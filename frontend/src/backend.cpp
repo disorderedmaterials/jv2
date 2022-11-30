d1707fb9d9b6fae034412d12131dc5e8f809d26e
Martyn Gigg <martyn.gigg@gmail.com>
2022-11-30 23:00:07 +0000

    The frontend now starts the backend process
    This alleviates a pain point in providing a
    cross-platform solution for starting both
    the frontend and backend.

---
+++ frontend/src/backend.cpp
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022 E. Devlin and T. Youngs
+#include "backend.h"
+#include <QDebug>
+
+/**
+ * Configure process for start to be called
+ */
+Backend::Backend()
+{
+    process_.setProgram("gunicorn");
+    QStringList args;
+    // clang-format off
+    args << "--bind" << Backend::bind_address()
+         << "--graceful-timeout" << "120"
+         << "--timeout" << "120"
+         << "jv2backend.app:create_app()";
+    // clang-format on
+    process_.setArguments(args);
+    process_.setProcessChannelMode(QProcess::ForwardedChannels);
+}
+
+/**
+ * Start the backend process and hold the process open
+ */
+void Backend::start()
+{
+    qDebug() << "Starting backend";
+    process_.start();
+    process_.waitForStarted();
+}
+
+/**
+ * Stop the process
+ */
+void Backend::stop()
+{
+    qDebug() << "Stopping backend.";
+    process_.terminate();
+    process_.waitForFinished();
+}
--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -1,14 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2022 E. Devlin and T. Youngs
 
+#include "backend.h"
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QProcess>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
+    // Configure the main application early to create an event loop
     QApplication application(argc, argv);
     application.setWindowIcon(QIcon(":/icon"));
+    // Start backend as soon as we can
+    Backend backend;
+    backend.start();
+    // A sleep is not ideal but the mainwindow needs the backend fully started
+    // before it can start
+    QThread::sleep(1);
+
+    // Start frontend and hook up kill backend on quit
+    QObject::connect(&application, &QApplication::aboutToQuit, &backend, &Backend::stop);
     MainWindow window;
     window.show();
     return application.exec();
