ef929ac75fb0e58a0ec367d5ddbb828f1231414b
Martyn Gigg <martyn.gigg@gmail.com>
2022-12-03 11:01:43 +0000

    Introduce environment variables to configure backend
    The variables allow users to configure elements of the
    backend required for their environment

--- backend/src/jv2backend/config.py
+++ backend/src/jv2backend/config.py
@@ -1,9 +1,26 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2022 E. Devlin, M. Gigg and T. Youngs
-"""Hold default configuration values for the program."""
+"""Access configuration values for the program"""
+import os
 
+ENVIRON_NAME_PREFIX = "JV2"
 
 DEFAULTS = {
+    "logger_level": "INFO",
     "journal_server_url": "http://data.isis.rl.ac.uk/journals",
+    "run_locator_class": "jv2backend.io.isis.filelocator.LegacyArchiveFileLocator",
     "run_locator_prefix": "/archive",
 }
+
+def get(name: str) -> str:
+    """Access a configuration value via its name.
+    Values can be overridden by defining environment
+    variables named JV2_{name.upper()}. If not
+    enviroment variable exists a default value is
+    returned.
+
+    :return: The configuration value
+    :raises: KeyError if not value can be found
+    """
+    environ_var_name = f"{ENVIRON_NAME_PREFIX}_{name.upper()}"
+    return os.environ.get(environ_var_name, DEFAULTS[name])
