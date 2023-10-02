d67a0b8d6267b6f25a51454df1e6560bc4e185b6
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-10-02 15:00:36 +0100

    But they are overrides.

--- frontend/jsonTableModel.h
+++ frontend/jsonTableModel.h
@@ -14,27 +14,11 @@
 class JsonTableModel : public QAbstractTableModel
 {
     public:
+    JsonTableModel();
+
     // Assigning custom data types for table headings
     typedef QMap<QString, QString> Heading;
     typedef QVector<Heading> Header;
-    JsonTableModel();
-
-    bool setJson(const QJsonArray &array);
-    QJsonArray getJson();
-    bool setHeader(const Header &array);
-    Header getHeader();
-
-    QJsonObject getJsonObject(const QModelIndex &index) const; // get row data
-
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-    void groupData();
-    void unGroupData();
-    void setColumnTitle(int section, QString title);
-    bool setData(const QModelIndex &index, QJsonObject rowData, int role = Qt::EditRole);
-    bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex());
 
     private:
     Header tableHeader_;
@@ -42,4 +26,27 @@ class JsonTableModel : public QAbstractTableModel
     Header tableGroupedHeader_;
     QJsonArray tableJsonData_;
     QJsonArray tableHoldJsonData_;
+
+    private:
+    bool setJson(const QJsonArray &array);
+    QJsonArray getJson();
+    bool setHeader(const Header &array);
+    Header getHeader();
+
+    QJsonObject getJsonObject(const QModelIndex &index) const; // get row data
+
+    void groupData();
+    void unGroupData();
+    void setColumnTitle(int section, QString title);
+    bool setData(const QModelIndex &index, QJsonObject rowData, int role = Qt::EditRole);
+
+    /*
+     * QAbstractTableModel Overrides
+     */
+    protected:
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
 };
