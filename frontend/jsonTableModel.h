dc81e011665c8e6c505b33327e134390fdd2ec3b
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-10-02 15:07:20 +0100

    Reorganise.

--- frontend/jsonTableModel.h
+++ frontend/jsonTableModel.h
@@ -40,9 +40,9 @@ class JsonTableModel : public QAbstractTableModel
      * QAbstractTableModel Overrides
      */
     public:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
     bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
 };
