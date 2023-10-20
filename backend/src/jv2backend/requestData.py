4d2b8657b58fe36c85a719b6671207b47303c669
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-10-20 20:24:44 +0100

    Simplify InvalidRequest exception.

--- backend/src/jv2backend/requestData.py
+++ backend/src/jv2backend/requestData.py
@@ -9,17 +9,12 @@ from jv2backend.utils import url_join
 class InvalidRequest(Exception):
     status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):
+    def __init__(self, message):
         Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+        self._message = message
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
+    def __str__(self):
+        return self._message
 
 
 class RequestData:
