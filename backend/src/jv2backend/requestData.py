ee1dc8cf6d8d243f93a5f42322c627ae41807b64
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-10-27 22:25:47 +0100

    Make data directory spec similar to journal file spec.

--- backend/src/jv2backend/requestData.py
+++ backend/src/jv2backend/requestData.py
@@ -42,7 +42,7 @@ class RequestData:
                                 given
             require_in_library: Whether a collection matching the library_key
                                 must already exist
-        require_data_directory: Whether a 'dataDirectory' must be provided
+        require_data_directory: Whether a 'runDataRootUrl' must be provided
               require_filename: Whether a 'filename' must be provided
            require_run_numbers: Whether one or more run numbers are expected
              require_parameter: Name of an additional parameter to expect
@@ -52,8 +52,7 @@ class RequestData:
         self._journal_root_url: str = None
         self._journal_filename: str = None
         self._directory: str = None
-        # OLD
-        self._data_directory: str = None
+        self._run_data_root_url: str = None
         self._parameter: str = None
         self._journal_collection: jv2backend.journals.JournalCollection = None
         self._run_numbers: typing.List[int] = []
@@ -92,9 +91,9 @@ class RequestData:
                                  f"in library.")
 
         # Was a data directory provided / required?
-        self._data_directory = (requestData["runDataRootUrl"]
+        self._run_data_root_url = (requestData["runDataRootUrl"]
                                 if "runDataRootUrl" in requestData else None)
-        if require_data_directory and self._data_directory is None:
+        if require_run_data_root_url and self._run_data_root_url is None:
             raise InvalidRequest("Data directory required but not given.")
 
         # Were run number(s) provided / required?
@@ -156,9 +155,17 @@ class RequestData:
         return self._directory
 
     @property
-    def data_directory(self) -> str:
-        """Return the data directory (if given)"""
-        return self._data_directory
+    def run_data_root_url(self) -> str:
+        """Return the root of the data directory (if given)"""
+        return self._run_data_root_url
+
+    @property
+    def run_data_url(self) -> str:
+        """Return the full path to the data directory (if given)"""
+        if self._directory:
+            return url_join(self._run_data_root_url, self._directory)
+        else:
+            return self._run_data_root_url
 
     @property
     def run_numbers(self) -> typing.List[int]:
