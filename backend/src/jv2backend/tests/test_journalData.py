7d11117d810ee307955da3d372478931c2bacae2
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-10-31 14:42:34 +0000

    Absorb old test_journal.py into JournalData tests.

--- backend/src/jv2backend/tests/test_journalData.py
+++ backend/src/jv2backend/tests/test_journalData.py
@@ -3,6 +3,7 @@
 
 from jv2backend.journals import JournalData
 import xml.etree.ElementTree as ET
+import json
 import pandas
 import pytest
 
@@ -38,3 +39,75 @@ def test_run_data_does_not_contain_run_number(run_number):
 
     assert run_number not in data
     assert data.run(run_number) is None
+
+
+@pytest.mark.parametrize("case_sensitive", [True, False])
+def test_search_by_user_name_field_uses_a_contains_check_not_exact_match(case_sensitive):
+    data = JournalData(runData)
+
+    search_results = data.search("user_name", "devlin", case_sensitive)
+
+    if case_sensitive:
+        assert search_results.run_count == 0
+    else:
+        assert search_results.run_count == 6
+
+
+@pytest.mark.parametrize("case_sensitive", [True, False])
+def test_search_by_experiment_identifier_uses_exact_string_matching(case_sensitive):
+    data = JournalData(runData)
+
+    search_results = data.search("experiment_identifier", "123456", case_sensitive)
+    assert search_results.run_count == 0
+
+    search_results = data.search("experiment_identifier", "1234567", case_sensitive)
+    assert search_results.run_count == 3
+
+
+@pytest.mark.parametrize("case_sensitive", [True, False])
+def test_search_by_title_uses_a_contains_check_and_not_exact_match(case_sensitive):
+    data = JournalData(runData)
+
+    search_results = data.search("title", "science", case_sensitive)
+    if case_sensitive:
+        assert search_results.run_count == 0
+    else:
+        assert search_results.run_count == 5
+
+
+def test_search_by_run_number_uses_a_range_check_assuming_user_gives_start_end():
+    data = JournalData(runData)
+
+    search_results = data.search("run_number", "10-1000")
+    assert search_results.run_count == 0
+
+    search_results = data.search("run_number", "1-5")
+    assert search_results.run_count == 3
+
+    runs = json.loads(search_results.to_json())
+
+    assert runs[0]["run_number"] == "1"
+    assert runs[1]["run_number"] == "2"
+    assert runs[2]["run_number"] == "3"
+
+
+def test_search_by_run_number_uses_a_range_check_given_range_operator():
+    data = JournalData(runData)
+
+    search_results = data.search("run_number", ">6")
+    assert search_results.run_count == 2
+
+    runs = json.loads(search_results.to_json())
+    assert runs[0]["run_number"] == "8"
+    assert runs[1]["run_number"] == "9"
+
+
+def test_search_by_start_time_treating_input_as_datetime_and_equivalent_to_start_date():
+    data = JournalData(runData)
+
+    search_results = data.search("start_time", "2023/02/03-2023/02/04")
+
+    assert search_results.run_count == 2
+    runs = json.loads(search_results.to_json())
+    assert runs[0]["run_number"] == "2"
+    assert runs[1]["run_number"] == "3"
