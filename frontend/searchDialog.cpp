5764ab9d82c2cd139b508826ef8a8c5e00d384bb
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-12-20 14:42:40 +0000

    ui: extend search everywhere dialog.

--- frontend/searchDialog.cpp
+++ frontend/searchDialog.cpp
@@ -2,8 +2,13 @@
 // Copyright (c) 2024 Team JournalViewer and contributors
 
 #include "searchDialog.h"
+#include <QIntValidator>
 
-SearchDialog::SearchDialog(QWidget *parent) : QDialog(parent) { ui_.setupUi(this); }
+SearchDialog::SearchDialog(QWidget *parent) : QDialog(parent)
+{
+    ui_.setupUi(this);
+    ui_.ExperimentIdentifierEdit->setValidator(new QIntValidator(0, 9999999, this));
+}
 
 void SearchDialog::on_CancelButton_clicked(bool checked) { reject(); }
 
@@ -17,8 +22,12 @@ std::map<QString, QString> SearchDialog::getQuery()
 
     // Assemble the search query parameters
     std::map<QString, QString> parameters;
+
+    // Run title
     if (ui_.RunTitleCheckBox->isChecked() && !ui_.RunTitleEdit->text().isEmpty())
         parameters["title"] = ui_.RunTitleEdit->text();
+
+    // Run Number
     if (ui_.RunNumberCheckBox->isChecked())
     {
         if (ui_.RunNumberRangeRadio->isChecked())
@@ -29,6 +38,14 @@ std::map<QString, QString> SearchDialog::getQuery()
         else if (ui_.RunNumberAfterRadio->isChecked())
             parameters["run_number"] = QString(">%1").arg(ui_.RunNumberAfterSpinBox->value());
     }
+
+    // User
+    if (ui_.UserCheckBox->isChecked() && !ui_.UserEdit->text().isEmpty())
+        parameters["user_name"] = ui_.UserEdit->text();
+
+    // Experiment Identifier
+    if (ui_.ExperimentIdentifierCheckBox->isChecked() && !ui_.ExperimentIdentifierEdit->text().isEmpty())
+        parameters["experiment_identifier"] = ui_.ExperimentIdentifierEdit->text();
 
     return parameters;
 }
